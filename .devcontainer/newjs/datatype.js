f5a7f8f579af0c37f82ca95d12a5e48f320ffa81
anshikaDD <manitripathianshika@gmail.com>
2024-05-18 08:52:20 +0000

    datatype.js

---
+++ .devcontainer/newjs/datatype.js
@@ -0,0 +1,15 @@
+"user strict";//treat all js code as a newer version//
+let age=18
+let password=12345
+let name="kumar"
+let state;
+let isloggedin=false
+//alert=3+3 we are using node js not browser//
+//number =2 to 35//
+//boolean=true/false//
+//bigint//
+//null=standalone value//
+//undefined//
+//symbol=>uniqueness//
+//object
+console.log(typeof password);
---
+++ .devcontainer/newjs/variable.js
@@ -0,0 +1,10 @@
+const accountId=14453
+let email="hitesh@google.com"
+var accountpassword="12345"
+accountcity="jaipur"
+/*not use var bcz of issue in block scope and functional scope */
+
+email="h.h.com"
+accountpassword="212121"
+accountcity="bengaluru"
+console.table([accountId,email,accountpassword,accountcity]);
